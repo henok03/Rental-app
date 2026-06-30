e12599fa3a47fc5dd78d5c889febb0e0656e7f7e
henok hailegnaw <henokhailegnaw88@gmail.com>
2026-06-30 12:28:14 +0300

    Secure supabase client with env variables

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// From your Supabase Project Settings -> API
-const supabaseUrl = 'https://eqdihokpyyxuikwwkbra.supabase.co'; 
-
-
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZGlob2tweXl4dWlrd3drYnJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3OTA1OTIsImV4cCI6MjA5NTM2NjU5Mn0.4I7DEJA0eJmOO_J1425btBgaZU_pdkWzH9_9RfXx0mo'; 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
